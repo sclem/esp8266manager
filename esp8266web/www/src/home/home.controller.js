2bb9fd9dd30d47e3a24d7d32dcda0a36ee85dfea
sclem <sclem@novetta.com>
2016-05-29 14:41:43 -0400

    Change to load event

--- esp8266web/www/src/home/home.controller.js
+++ esp8266web/www/src/home/home.controller.js
@@ -1,8 +1,12 @@
-app.controller('HomeCtrl', function($scope, $state, $http) {
+app.controller('HomeCtrl', function($scope, $state, $http, $ionicHistory) {
 
     $scope.modules = [];
 
-    $scope.$on('$ionicView.enter', function() {
+    $scope.$on('$ionicView.afterEnter', function() {
+        $ionicHistory.clearHistory();
+    });
+
+    $scope.$on('$ionicView.loaded', function() {
         $http.get('/modules').then(function(resp) {
             $scope.modules = resp.data.map(function(item) {
                 return {
